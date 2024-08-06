e221bc840756336ff94702e485a846cceec5bf12
CodingManoj <CodingManoj@gmail.com>
2024-08-05 21:40:50 -0400

    Auto Push

--- 001-frontend.sh
+++ 001-frontend.sh
@@ -8,16 +8,26 @@ if [ "$ID" -ne 0 ]; then
     exit 1
 fi 
 
+echo "Installing Nginx"
 dnf install nginx -y  
+
+echo "Copying Proxy file"
 cp proxy.conf /etc/nginx/default.d/expense.conf 
-systemctl enable nginx    
+
+echo "Enabling Nginx"
+systemctl enable nginx
+
+echo "Performing a Cleanup"
 rm -rf /usr/share/nginx/html/*   
+
+echo "Downloading Frontend"
 curl -o /tmp/frontend.zip https://expense-web-app.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html 
-unzip /tmp/frontend.zip
-pwd
-ls -ltr
 
+echo "Extracting frontend"
+unzip -o /tmp/frontend.zip
+
+echo "Starting frontend"
 systemctl restart nginx 
 
 echo "** Frontend Installation Is Completed **"
