2ebb607c0e6fde0b7ae3827b4e066ea43baef589
CodingManoj <CodingManoj@gmail.com>
2024-08-05 22:06:31 -0400

    Auto Push

---
+++ 002-mysql.sh
@@ -0,0 +1,38 @@
+#!/bin/bash 
+
+COMPONENT="mysql"
+ID=$(id -u)
+LOG="/tmp/mysql.log"
+
+COLOR() {
+    echo -e "\e[35m $* \e[0m"
+} 
+
+stat() {
+    if [ $1 -eq  0 ] ; then 
+        echo -e "\e[32m - Success \e[0m" 
+    else 
+        echo -e "\e[31m - Failure \e[0m" 
+        exit 1
+    fi 
+}
+
+if [ "$ID" -ne 0 ]; then 
+    echo -e "\e[31m Script is expected to be executed as a root user or with sudo scriptName.sh \e[0m"
+    echo -e "\t  sudo bash $0"
+    exit 1
+fi 
+
+COLOR Installing $COMPONENT
+dnf install mysql-server -y  &>> $LOG
+
+COLOR Enabling $COMPONENT
+systemctl enable mysqld       &>> $LOG
+
+COLOR Starting $COMPONENT 
+systemctl start  mysqld        &>> $LOG      
+
+COLOR Configuring $COMPONENT Root Password
+mysql_secure_installation --set-root-pass ExpenseApp@1
+
+echo -e "\n\t ** Mysql Installation Completed **"
