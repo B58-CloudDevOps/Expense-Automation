1293fff750a88d49df55c41958f48cc24e94f54a
CodingManoj <CodingManoj@gmail.com>
2024-08-06 20:00:56 -0400

    Auto Push

--- 002-mysql.sh
+++ 002-mysql.sh
@@ -33,12 +33,11 @@ systemctl enable mysqld       &>> $LOG
 stat $?
 
 COLOR Starting $COMPONENT 
-systemctl start  mysqld        &>> $LOG      
+systemctl start  mysqld       &>> $LOG      
 stat $?
 
-
 COLOR Configuring $COMPONENT Root Password
-mysql_secure_installation --set-root-pass $ROOTPASS
+mysql_secure_installation --set-root-pass $ROOTPASS  &>> $LOG      
 stat $?
 
 # echo "show databases;" | mysql -uroot -pExpenseApp@1 
